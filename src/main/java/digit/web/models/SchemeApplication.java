4a57ee7e49c074c5191838282f52a29465f0df0a
SundeepBose <167606266+SundeepBose@users.noreply.github.com>
2024-06-08 14:30:35 +0530

    As per digit

---
+++ src/main/java/digit/web/models/SchemeApplication.java
@@ -0,0 +1,73 @@
+package digit.web.models;
+
+import org.egov.common.contract.models.AuditDetails;
+import org.egov.common.contract.request.User;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * Represents a scheme application entity.
+ */
+@ToString
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SchemeApplication {
+
+    @JsonProperty("id")
+    private Long id;
+
+    @JsonProperty("userId")
+    private Long userId;
+
+    @JsonProperty("tenantId")
+    private String tenantId;
+
+    @JsonProperty("optedId")
+    private Integer optedId;
+
+    @JsonProperty("applicationStatus")
+    private Boolean applicationStatus;
+
+    @JsonProperty("verificationStatus")
+    private Boolean verificationStatus;
+
+    @JsonProperty("firstApprovalStatus")
+    private Boolean firstApprovalStatus;
+
+    @JsonProperty("randomSelection")
+    private Boolean randomSelection;
+
+    @JsonProperty("finalApproval")
+    private Boolean finalApproval;
+
+    @JsonProperty("submitted")
+    private Boolean submitted;
+
+    @JsonProperty("modifiedOn")
+    private Long modifiedOn;
+
+    @JsonProperty("createdBy")
+    private String createdBy;
+
+    @JsonProperty("modifiedBy")
+    private String modifiedBy;
+
+    @JsonProperty("address")
+    private Address address;
+
+    @JsonProperty("auditDetails")
+    private AuditDetails auditDetails;
+
+    @JsonProperty("user")
+    private User user;
+}
